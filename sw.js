eb4913bb049fc11e7f7d86dccac53e5e1795e3a0
jonathan silva <silvajo1.ac@gmail.com>
2026-09-02 00:54:14 -0300

    chore: bump service worker cache and add activate cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'centro-padres-v1';
+const CACHE_NAME = 'centro-padres-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -27,5 +27,20 @@ self.addEventListener('fetch', event => {
         }
         return fetch(event.request);
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
